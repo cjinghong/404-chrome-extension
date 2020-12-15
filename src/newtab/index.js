f22cc775c2021581b5ca5189e8c2e83e4e412535
Chan Jing Hong <jinghongchan@gmail.com>
2020-12-15 09:42:09 +0800

    updated eslintconfig and package. added new tab component

---
+++ src/newtab/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import NewTab from './NewTab';
+
+ReactDOM.render(<NewTab />, document.getElementById('root'));
+
